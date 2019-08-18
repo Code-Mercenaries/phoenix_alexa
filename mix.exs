3b4b6881145a5640e481bf930e129973bc9ff0cd
Andrei Fedianov <andreifaa@gmail.com>
2019-08-18 13:25:36 -0400

    💾

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule PhoenixAlexa.MixProject do
   use Mix.Project
 
-  @version File.read!("VERSION") |> String.strip()
+  @version File.read!("VERSION") |> String.trim()
 
   def project do
     [
